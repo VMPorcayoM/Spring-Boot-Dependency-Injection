0e0e8322d145a40a1636d215b12d7a79719455d7
Victor Miguel Porcayo <victor.porcayo@handcloud.com.mx>
2022-07-24 15:40:21 -0500

    First commit

---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,9 @@
+package com.bolsadeideas.springboot.di.app.models.services;
+
+public class MiServicio {
+
+    public String operacion(){
+        return "Algun proceso importante...";  git
+    }
+    
+}
